684972cf10590e9f060925c67299c42d2e5bab0c
Jonas Juselius <jonas.juselius@uit.no>
2012-10-25 15:32:31 +0200

    Added some old examples.

---
+++ Parallel/mpiex.py
@@ -0,0 +1,25 @@
+#
+# Simple scatter-gather example using mpi4py
+#
+# $ mpirun -np 4 python ./mpiex.py
+#
+import numpy as np
+from mpi4py import MPI
+
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+size = comm.Get_size()
+
+if rank == 0:
+    data = [(i+1)**2 for i in xrange(size)]
+else:
+   data = None
+
+print 'foo ', rank, data
+
+data = comm.scatter(data, root=0)
+print 'rank=', rank, data
+data += 100
+data = comm.gather(data, root=0)
+
+print 'rank=', rank, data
