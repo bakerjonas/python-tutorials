acfc269c822eed22d35d5c701997856058f6daa5
Jonas Juselius <jonas.juselius@uit.no>
2013-10-08 12:07:40 +0200

    Basic debugging skills.

--- Debugging/fib_bug.py
+++ Debugging/fib_bug.py
@@ -1,24 +1,10 @@
-#
-#
-"""
-Simple bug.
-"""
-
-class Apa(object):
-    """
-    yo yo
-    """
-    def fib2(self, n = 1000):
-        """
-        Hey hey
-        """
-        print self.fib2.__doc__, Apa.__doc__
-        a, b = 0, 1
-        result = []
-        while a < n:
-            result.append(a)
-            a, b = a, a+b
-        return result, a, b
+def fib2(n = 1000):
+    a, b = 0, 1
+    result = []
+    while a < n:
+        result.append(a)
+        a, b = a, a+b
+    return result, a, b
 
 if __name__ == '__main__':
-    Apa().fib2()
+    print fib2(10)
